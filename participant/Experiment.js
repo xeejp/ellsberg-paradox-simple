fe21ff0b699048e8bc154a58335b8f3e1cbfaf91
koo3701 <koichiro.koo.gin@docomo.ne.jp>
2016-08-25 06:55:21 +0900

    add SwipeableViews

--- participant/Experiment.js
+++ participant/Experiment.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
 import RaisedButton from 'material-ui/RaisedButton'
+import SwipeableViews from 'react-swipeable-views'
 
 import { nextQuestion } from './actions'
 
@@ -15,11 +16,17 @@ const mapStateToProps = ({ sequence, qswap }) => ({
 class Experiment extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      slideIndex: 0
+    }
   }
 
   next(value) {
     const{ dispatch } = this.props
     dispatch(nextQuestion(value))
+    this.setState({
+      slideIndex: 1
+    })
   }
   
   render() {
@@ -29,9 +36,18 @@ class Experiment extends Component {
     return (sequence != "answered")?
       <div style={{height: 'auto'}}>
         <h5>{Question.text}</h5>
-        <p>{Text.text}</p>
-        <RaisedButton label={Text.question[0]} onClick={this.next.bind(this, 1)} style={{float:  'left', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
-        <RaisedButton label={Text.question[1]} onClick={this.next.bind(this, 2)} style={{float: 'right', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
+        <SwipeableViews index={this.state.slideIndex} disabled={true}>
+          <div>
+            <p>{Text.text}</p>
+            <RaisedButton label={Text.question[0]} onClick={this.next.bind(this, 1)} style={{float:  'left', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
+            <RaisedButton label={Text.question[1]} onClick={this.next.bind(this, 2)} style={{float: 'right', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
+          </div>
+          <div>
+            <p>{Text.text}</p>
+            <RaisedButton label={Text.question[0]} onClick={this.next.bind(this, 1)} style={{float:  'left', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
+            <RaisedButton label={Text.question[1]} onClick={this.next.bind(this, 2)} style={{float: 'right', width: '48%', height: '300px', position: 'relative'}} labelStyle={{position: 'absolute', top: '50%', left: '50%', width: '100%', margin: '-1.5em 0 0 -50%'}} />
+          </div>
+        </SwipeableViews>
       </div>
     : <div><p>{Text.text}</p></div>
   }
